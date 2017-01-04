641d8849a31e716dd37955ccc0e379199728027d
jiangwei37 <jiangwei5@jd.com>
2017-01-04 15:15:32 +0800

    左侧列表

--- src/scripts/layout/Sidebar.js
+++ src/scripts/layout/Sidebar.js
@@ -1,22 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { Sidebar, Nav, Icon } from 'react-impression';
+import { Sidebar, Nav, Icon,Button } from 'react-impression';
+import routesConfig from './routes.js';
 
+//侧边栏
+export default class AppSidebar extends Component {
+
+    onClickHandle = () => {
+
+    }
+    render() {
+        const { decrement, counter } = this.props;
+
+        //先写一个循环试试
+      //   const extractRoutes = config => {
+      //     const routes = [];
+      //
+      //     config.forEach(group => {
+      //         group.children.forEach(item => {
+      //             const { path, name } = item;
+      //             routes.push(<Nav.Link><Link to={item.path}><Icon type={item.name} left />{item.name}</Link></Nav.Link>)
+      //         });
+      //     });
+      //
+      //     return routes;
+      // };
+
+      //const routes = extractRoutes(routesConfig);
+
+        return (
+          <Sidebar>
+              <Sidebar.Header />
+              <Sidebar.Body>
+              {
+                routesConfig.map(group => {
+                  return (
+                    <Nav>
+                      <div className="nav-title">
+                      <Button theme="" onClick={this.onClickHandle}>{group.title}
+                      </Button>
+                      </div>
+                      {
+                        group.children.map(item => {
+                          return (
+                            <Nav.Link><Link to={item.path}><Icon type={item.icon} left />{item.name}</Link></Nav.Link>
+                          );
+                        })
+                      }
+                    </Nav>
+                  );
+                })
+              }
+              </Sidebar.Body>
+          </Sidebar>
+        );
+    }
+}
 
 // 侧边栏Sidebar
-const AppSidebar = () => {
-    return (
-        <Sidebar>
-            <Sidebar.Header />
-            <Sidebar.Body>
-                <Nav>
-                    <Nav.Link>
-                        <Link to="/index"><Icon type="television" left />Counter</Link>
-                    </Nav.Link>
-                </Nav>
-            </Sidebar.Body>
-        </Sidebar>
-    );
-};
+// const AppSidebar = () => {
+//     return (
+//         <Sidebar>
+//             <Sidebar.Header />
+//               <Button theme="primary" onClick="{this.onClickHandle}" left>click me</Button>
+//             <Sidebar.Body>
+//                 <Nav>
+//                     <Nav.Link>
+//                         <Link to="/index"><Icon type="television" left />Counter</Link>
+//                     </Nav.Link>
+//                     <Nav.Link>
+//                         <Link to="/home"><Icon type="television" left />Counter</Link>
+//                     </Nav.Link>
+//                     <Nav.Link>
+//                         <Link to="/index"><Icon type="television" left />Counter</Link>
+//                     </Nav.Link>
+//                 </Nav>
+//             </Sidebar.Body>
+//         </Sidebar>
+//     );
+// };
 
-export default AppSidebar;
+
+// export default AppSidebar;
---
+++ src/scripts/layout/routes.js
@@ -0,0 +1,197 @@
+export default[
+    {
+        "title": "Base",
+        "children": [
+            {
+                "path": "/button",
+                "name": "Button",
+                "icon": "hand-pointer-o"
+            },
+            {
+                "path": "/icon",
+                "name": "Icon",
+                "icon": "flag"
+            },
+            {
+                "path": "/cell",
+                "name": "Group && Cell",
+                "icon": "list"
+            },
+            {
+                "path": "/flex",
+                "name": "Flex && FlexItem",
+                "icon": "th"
+            },
+            {
+                "path": "/tag",
+                "name": "Tag",
+                "icon": "tag"
+            },
+            {
+                "path": "/badge",
+                "name": "Badge",
+                "icon": "bell-o"
+            },
+            {
+                "path": "/hrule",
+                "name": "HRule(Hr)",
+                "icon": "minus"
+            },
+            {
+                "path": "/tip",
+                "name": "Tip",
+                "icon": "paperclip"
+            },
+            {
+                "path": "/card",
+                "name": "Card",
+                "icon": "file-image-o"
+            },
+            {
+                "path": "/media",
+                "name": "Media",
+                "icon": "id-card-o"
+            },
+            {
+                "path": "/swipe",
+                "name": "Swipe",
+                "icon": "newspaper-o"
+            }
+        ]
+    },
+    {
+        "title": "Layout",
+        "children": [
+            {
+                "path": "/navbar",
+                "name": "Navbar",
+                "icon": "window-maximize"
+            },
+            {
+                "path": "/tabbar",
+                "name": "Tabbar",
+                "icon": "clone"
+            },
+            {
+                "path": "/navigation",
+                "name": "Navigation",
+                "icon": "tablet"
+            },
+            {
+                "path": "/drawer",
+                "name": "Drawer",
+                "icon": "caret-square-o-down"
+            },
+            {
+                "path": "/segmented-control",
+                "name": "SegmentedControl",
+                "icon": "columns"
+            },
+            {
+                "path": "/slideup",
+                "name": "SlideUp",
+                "icon": "angle-double-up"
+            },
+            {
+                "path": "/sideslip",
+                "name": "Sideslip",
+                "icon": "window-restore"
+            },
+            {
+                "path": "/searchbar",
+                "name": "Searchbar",
+                "icon": "search"
+            },
+            {
+                "path": "/picker",
+                "name": "Picker",
+                "icon": "building-o"
+            },
+            {
+                "path": "/pull-down",
+                "name": "Pull down",
+                "icon": "long-arrow-down"
+            },
+            {
+                "path": "/pull-up",
+                "name": "Pull up",
+                "icon": "long-arrow-up"
+            }
+        ]
+    },
+    {
+        "title": "prompt",
+        "children": [
+            {
+                "path": "/toast",
+                "name": "Toast",
+                "icon": "commenting-o"
+            },
+            {
+                "path": "/alert",
+                "name": "Alert",
+                "icon": "bullhorn"
+            },
+            {
+                "path": "/confirm",
+                "name": "Confirm",
+                "icon": "question-circle-o"
+            },
+            {
+                "path": "/loading",
+                "name": "loading",
+                "icon": "spinner"
+            },
+            {
+                "path": "/back-to-top",
+                "name": "BackToTop",
+                "icon": "arrow-circle-up"
+            }
+        ]
+    },
+    {
+        "title": "form",
+        "children": [
+            {
+                "path": "/checkbox",
+                "name": "Checkbox",
+                "icon": "check-square"
+            },
+            {
+                "path": "/radio",
+                "name": "Radio",
+                "icon": "dot-circle-o"
+            },
+            {
+                "path": "/toggle",
+                "name": "Toggle(Switch)",
+                "icon": "toggle-on"
+            },
+            {
+                "path": "/input-text",
+                "name": "InputText",
+                "icon": "font"
+            },
+            {
+                "path": "/input-number",
+                "name": "InputNumber",
+                "icon": "plus-circle"
+            },
+            {
+                "path": "/input-textarea",
+                "name": "InputTextarea",
+                "icon": "file-text-o"
+            },
+            {
+                "path": "/selector",
+                "name": "Selector",
+                "icon": "check"
+            },
+            {
+                "path": "/inline-selecor",
+                "name": "InlineSelector",
+                "icon": "th-large"
+            }
+        ]
+    }
+]
