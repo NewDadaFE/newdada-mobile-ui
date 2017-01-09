975cedaccfc11ded86f6fc6a071b08e80f504147
Ivan <wcy5211678@163.com>
2017-01-09 10:57:43 +0800

    add _theme

---
+++ _theme/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  home: '/',
+  sitename: 'One',
+  // tagline: 'The one theme for bisheng',
+  // navigation: [{
+  //   title: 'BiSheng',
+  //   link: 'https://github.com/benjycui/bisheng',
+  // }],
+  // footer: 'Copyright and so on...',
+  // hideBisheng: true,
+  github: 'https://github.com/benjycui/bisheng-theme-one',
+  routes: [{
+    path: '/',
+    component: './template/Archive',
+  }, {
+    path: '/posts/:post',
+    component: './template/Post',
+  }, {
+    path: '/tags',
+    component: './template/TagCloud',
+    dataPath: '/',
+  }],
+};
