75b20638841f510b23d40499741740922dd9da92
Ivan <wcy5211678@163.com>
2017-01-12 18:09:38 +0800

    antd

--- site/_theme/template/Home/index.js
+++ site/_theme/template/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import DocumentTitle from 'react-document-title'
 import { Link } from 'react-router'
 import GitHubButton from 'react-github-button'
-
+import 'react-github-button/assets/style.css'
 
 export default class Home extends React.Component {
     componentWillMount() {
