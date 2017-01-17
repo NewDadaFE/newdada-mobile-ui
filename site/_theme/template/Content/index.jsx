00030b63f15514defa88a1325fdbe2ba6c744c38
Ivan <wcy5211678@163.com>
2017-01-17 09:32:41 +0800

    locale

--- site/_theme/template/Content/index.jsx
+++ site/_theme/template/Content/index.jsx
@@ -9,28 +9,29 @@ const locale = (
 ) ? 'zh-CN' : 'en-US';
 
 export function collect(nextProps, callback) {
-  const pageData = nextProps.location.pathname === 'changelog' ?
-    nextProps.data.CHANGELOG : nextProps.pageData;
-  const pageDataPromise = typeof pageData === 'function' ?
-    pageData() : (pageData[locale] || pageData.index[locale] || pageData.index)();
-  const promises = [pageDataPromise];
+    const pageData = nextProps.location.pathname === 'changelog' ?
+        nextProps.data.CHANGELOG : nextProps.pageData;
+    const pageDataPromise = typeof pageData === 'function' ?
+        pageData() : (pageData[locale] || pageData.index[locale] || pageData.index)();
+    const promises = [pageDataPromise];
 
-  const pathname = nextProps.location.pathname;
-  const demos = nextProps.utils.get(
-    nextProps.data, [...pathname.split('/'), 'demo']
-  );
-  if (demos) {
-    promises.push(demos());
-  }
-  Promise.all(promises)
-    .then((list) => callback(null, {
-      ...nextProps,
-      localizedPageData: list[0],
-      demos: list[1],
-    }));
+    const pathname = nextProps.location.pathname;
+    const demos = nextProps.utils.get(
+        nextProps.data, [...pathname.split('/'), 'demo']
+    );
+
+    if (demos) {
+        promises.push(demos());
+    }
+    Promise.all(promises)
+        .then((list) => callback(null, {
+            ...nextProps,
+            localizedPageData: list[0],
+            demos: list[1],
+        }));
 }
 
 
 export default (props) => (
-  <MainContent {...props} />
+    <MainContent {...props} />
 );
