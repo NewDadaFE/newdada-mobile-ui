0dcaf7ac55c31ec077ff5d060a91ac4175261f16
Ivan <wcy5211678@163.com>
2017-02-08 17:27:05 +0800

    Updates

--- components/button/index.md.js
+++ components/button/index.md.js
@@ -17,9 +17,9 @@ webpackJsonp([6,8],{
 	  },
 	  "toc": ["ul", ["li", ["a", {
 	    "className": "bisheng-toc-h2",
-	    "href": "#API-(-适用平台：WEB、React-Native-)"
-	  }, "API ( 适用平台：WEB、React-Native )"]]],
-	  "api": ["section", ["h2", "API ( 适用平台：WEB、React-Native )"], ["table", ["thead", ["tr", ["th", "成员"], ["th", "说明"], ["th", "类型"], ["th", "默认值"]]], ["tbody", ["tr", ["td", "type"], ["td", "按钮类型，可选值为", ["code", "primary"], "/", ["code", "ghost"], "/", ["code", "warning"], "或者不设"], ["td", "string"], ["td", "-"]], ["tr", ["td", "size"], ["td", "设置按钮大小，可选值为", ["code", "large"], "、", ["code", "small"]], ["td", "string"], ["td", ["code", "large"]]], ["tr", ["td", "activeStyle"], ["td", "禁止点击反馈效果(设为 false)，也可设置为自己的点击 style"], ["td", "false/{}"], ["td", "{}"]], ["tr", ["td", "disabled"], ["td", "是否不可用"], ["td", "boolean"], ["td", "false"]], ["tr", ["td", "onClick"], ["td", "点击按钮的回调函数"], ["td", "(e: Object): void"], ["td", "无"]], ["tr", ["td", "inline (", ["code", "WEB only"], ")"], ["td", "是否是行内按钮"], ["td", "boolean"], ["td", "false"]], ["tr", ["td", "across (", ["code", "WEB only"], ")"], ["td", "是否通栏展示"], ["td", "boolean"], ["td", "false"]], ["tr", ["td", "loading (", ["code", "WEB only"], ")"], ["td", "设置按钮载入状态"], ["td", "boolean"], ["td", "false"]], ["tr", ["td", "icon (", ["code", "WEB only"], ")"], ["td", "可以是 Icon 组件里包含的 icon 的名字，也可以是 require 本地 svg 图标 (注意：loading 设置后此项设置失效)"], ["td", "string/require(./local.svg)"], ["td", "-"]], ["tr", ["td", "onPressIn (", ["code", "RN only"], ")"], ["td", "同 RN TouchableHighlight onPressIn"], ["td", "(e: Object): void"], ["td", "无"]], ["tr", ["td", "onPressOut (", ["code", "RN only"], ")"], ["td", "同 RN TouchableHighlight onPressOut"], ["td", "(e: Object): void"], ["td", "无"]], ["tr", ["td", "onShowUnderlay (", ["code", "RN only"], ")"], ["td", "同 RN TouchableHighlight onShowUnderlay"], ["td", "(e: Object): void"], ["td", "无"]], ["tr", ["td", "onHideUnderlay (", ["code", "RN only"], ")"], ["td", "同 RN TouchableHighlight onHideUnderlay"], ["td", "(e: Object): void"], ["td", "无"]]]]]
+	    "href": "#API"
+	  }, "API"]]],
+	  "api": ["section", ["h2", "API"], ["table", ["thead", ["tr", ["th", "成员"], ["th", "说明"], ["th", "类型"], ["th", "默认值"]]], ["tbody", ["tr", ["td", "type"], ["td", "按钮类型，可选值为", ["code", "primary"], "/", ["code", "ghost"], "/", ["code", "warning"], "或者不设"], ["td", "string"], ["td", "-"]], ["tr", ["td", "size"], ["td", "设置按钮大小，可选值为", ["code", "large"], "、", ["code", "small"]], ["td", "string"], ["td", ["code", "large"]]]]]]
 	};
 
 /***/ }
